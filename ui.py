eb3c6322808a974d0ae428a680b2f554a5344ea2
Joe Downs <joe@dwns.dev>
2024-12-02 14:59:23 -0500

    WIP: swap green and blue pins

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 from RGBMatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
+import board
 
 image = Image.open("gradient.png")
 
@@ -8,7 +9,14 @@ options.rows = 32
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+#options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.rgb_pins=[
+    board.MTX_R1,
+    board.MTX_B1,
+    board.MTX_G1,
+    board.MTX_R2,
+    board.MTX_B2,
+    board.MTX_G2]
 
 matrix = RGBMatrix(options = options)
 
