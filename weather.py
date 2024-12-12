8cccdb8c425732c492d3314eb05e0f3ddb902d90
Joe Downs <joe@dwns.dev>
2024-12-11 20:21:01 -0500

    Use airport codes for cities

--- weather.py
+++ weather.py
@@ -1,19 +1,52 @@
 import datetime as dt
 import requests
 
+import airportsdata
+from data_models import Condition
+
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
+forecast_url = "http://api.openweathermap.org/data/2.5/forecast?"
+
 api_key = "d82a646fba41dcfeebe68f577c763207"
-cities = ["Louisville", "Boston", "San Francisco"]
+#cities = ["Louisville", "Boston", "San Francisco"]
+airports = airportsdata.load('IATA')
+cities = ["SDF", "BOS", "SFO", "AMS"]
 data = {}
+
+def convert_condition(condition):
+    if condition < 200:
+        return Condition.UNKNOWN
+    elif condition < 300:
+        return Condition.STORMY
+    elif condition < 600:
+        return Condition.RAINY
+    elif condition < 700:
+        return Condition.SNOWY
+    elif condition == 800:
+        return Condition.CLEAR_DAY
+    elif condition <= 802:
+        return Condition.PARTLY_CLOUDY_DAY
+    elif condition <= 804:
+        return Condition.CLOUDY
+    else:
+        return Condition.UNKNOWN
+    return
 
 def kelvin_to_celsius_fahrenheit(kelvin):
     celsius = kelvin - 273.15
     fahrenheit = celsius * (9/5) + 32
     return celsius, fahrenheit
 
+def get_lat_lon(airport):
+    lat = airports[airport]["lat"]
+    lon = airports[airport]["lon"]
+    return lat, lon
+
 for city in cities:
-    url = base_url + "appid=" + api_key + "&q=" + city
+    lat, lon = get_lat_lon(city)
+    url = f"{base_url}appid={api_key}&lat={lat}&lon={lon}"
     response = requests.get(url).json()
+    #print(response)
 
     temp_kelvin = response['main']['temp']
     temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
@@ -23,12 +56,27 @@ for city in cities:
     description = response['weather'][0]['description']
     wind_speed = response['wind']['speed']
 
+    url = f"{forecast_url}appid={api_key}&lat={lat}&lon={lon}"
+    response = requests.get(url).json()
+    conditions = list()
+
+    today = dt.datetime.now() + dt.timedelta(days=-1)
+    #print(response)
+
+    for forecast in response["list"]:
+        date = dt.datetime.fromtimestamp(forecast["dt"])
+        if date.day == today:
+            today = today + dt.timedelta(days=1)
+            continue
+        conditions.append(convert_condition(forecast["weather"][0]["id"]))
+
     data[city] = {
         "Temperature (°F)": f"{temp_fahrenheit:.0f}",
         "Temperature Feels Like (°F)": f"{feels_like_fahrenheit:.0f}",
         "Humidity (%)": humidity,
         "Wind Speed (m/s)": f"{wind_speed:.0f}",
-        "Weather Description": description
+        "Weather Description": description,
+        "Daily Conditions": conditions
     }
 
 print(data)
