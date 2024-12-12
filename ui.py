2aa5ba639f45f7d6ca99a6c4b4a21f3664e91a04
Joe Downs <joe@dwns.dev>
2024-12-11 20:22:26 -0500

    Use buttons to navigate interface

--- ui.py
+++ ui.py
@@ -1,17 +1,37 @@
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
 
-# import board
-
 import assets
 import ui_models
-from data_models import Condition, WeatherData, StockData
-# import stocks
+from data_models import WeatherData, StockData
+import stocks
 import f1_api
+import weather
+from weather import data
 
+
+# ================================ Button Setup ================================
+import board
+import digitalio
+
+buttons = []
+
+enter_button = digitalio.DigitalInOut(board.C0)
+buttons.append(enter_button)
+next_button = digitalio.DigitalInOut(board.C1)
+buttons.append(next_button)
+prev_button = digitalio.DigitalInOut(board.C2)
+buttons.append(prev_button)
+
+selection = 0
+
+for button in buttons:
+    button.direction = digitalio.Direction.INPUT
+
+# ================================ Matrix Setup ================================
 options = RGBMatrixOptions()
 options.rows = 32
 options.cols = 64
@@ -28,17 +48,23 @@ options.hardware_mapping = "adafruit-hat"  # If you have an Adafruit HAT: 'adafr
 
 matrix = RGBMatrix(options=options)
 
-weathers = [int] * 5
+# =============================== Data Collection ==============================
+weather_array = list()
 
-for i in range(5):
-    date = datetime.fromisoformat(f"2024-05-0{i+1}")
-    weather = WeatherData(Condition(i + 1), date, location="SDF")
-    weathers[i] = weather
+for city in weather.cities:
+    city_data = data[city]
+    date = datetime.now()
+    conditions = list()
+    for condition in city_data["Daily Conditions"]:
+        weather = WeatherData(condition, date, location=city)
+        date = date + timedelta(days=1)
+        conditions.append(weather)
+    weather_array.append(conditions)
 
-# stock_array = []
-# for ticker, info in stocks.topStocksDict.items():
-#     timeseries = info["History"]["Open"]
-#     stock_array.append(StockData(ticker, timeseries))
+stock_array = []
+for ticker, info in stocks.topStocksDict.items():
+    timeseries = info["History"]["Open"]
+    stock_array.append(StockData(ticker, timeseries))
 
 driver_standings = f1_api.get_driver_standings(1978)['DriverStandings']
 constructor_standings = f1_api.get_constructor_standings(1978)['ConstructorStandings']
@@ -47,27 +73,30 @@ top_3_constructors = list(map(lambda constructor: (f1_api.constructor_name_to_id
 print(top_3_drivers)
 print(top_3_constructors)
 
-weather_home = ui_models.WeatherScreen(image_file="assets/weather-frame.png", data=weathers)
-# stock_home = ui_models.StockScreen(image_file="assets/stock-frame.png", data=stock_array)
-f1_home = ui_models.F1Screen(image_file="assets/f1-frame.png", data=(top_3_drivers, top_3_constructors))
+weather_screen = ui_models.WeatherScreen(image_file="assets/weather-frame.png", data=weather_array)
+stock_screen = ui_models.StockScreen(image_file="assets/stock-frame.png", data=stock_array)
+f1_screen = ui_models.F1Screen(image_file="assets/f1-frame.png", data=(top_3_drivers, top_3_constructors))
 
-weather_home.toggle_selection()
-f1_home.toggle_selection()
-# weather_home.next_selection()
-# weather_home.update_weather(weathers)
-# matrix.SetImage(weather_home.image)
-# matrix.SetImage(stock_home.image)
-matrix.SetImage(f1_home.image)
+selections = [weather_screen, stock_screen, f1_screen]
 
-# while True:
-#     pass
-
-
-for _ in range(100):
-    sleep(1)
-    # weather_home.next_selection()
-    # weather_home.next_selection()
-    # weather_home.prev_selection()
-    # stock_home.next_selection()
-    f1_home.next_selection()
-    matrix.SetImage(f1_home.image)
+primary_selection = True
+secondary_selection = False
+while True:
+    if primary_selection:
+        if next_button.value:
+            selection += 1
+            selection = selection % len(selections)
+        elif prev_button.value:
+            selection -= 1
+            selection = selection % len(selections)
+    else:
+        if next_button.value:
+            selections[selection].next_selection()
+        elif prev_button.value:
+            selections[selection].prev_selection()
+    if enter_button.value:
+        selections[selection].toggle_selection()
+        secondary_selection = not secondary_selection
+        primary_selection = not primary_selection
+    sleep(0.1)
+    matrix.SetImage(selections[selection].image)
