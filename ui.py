700c8d86a192559189844e46728d521259897cc4
Joe Downs <joe@dwns.dev>
2024-12-11 21:12:48 -0500

    Swap green and blue pins

--- ui.py
+++ ui.py
@@ -38,13 +38,7 @@ options.cols = 64
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = "adafruit-hat"  # If you have an Adafruit HAT: 'adafruit-hat'
-# options.rgb_pins=[
-#     board.MTX_R1,
-#     board.MTX_B1,
-#     board.MTX_G1,
-#     board.MTX_R2,
-#     board.MTX_B2,
-#     board.MTX_G2]
+options.rgb_pins=[5, 6, 13, 12, 23, 16]
 
 matrix = RGBMatrix(options=options)
 
